b27393f8920fd6ebfea2a86de1eabef898496e8d
cegerard <git@eriatand.org>
2023-09-22 17:19:31 +0200

    Upload and display recipes' images

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,7 @@ class Recipe < ApplicationRecord
   validates :title, presence: true
 
   has_many :ingredients, through: :ingredients_recipes
+  has_one_attached :image
 
   def self.all_title_id
     Recipe.all.pluck(:title, :id)
