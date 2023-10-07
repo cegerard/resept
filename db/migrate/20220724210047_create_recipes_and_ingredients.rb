7c2f653b018c3bed09e914a23ad6533d6dd23069
cegerard <git@eriatand.org>
2023-10-07 15:05:07 +0200

    fix cops

--- db/migrate/20220724210047_create_recipes_and_ingredients.rb
+++ db/migrate/20220724210047_create_recipes_and_ingredients.rb
@@ -2,13 +2,11 @@
 
 class CreateRecipesAndIngredients < ActiveRecord::Migration[7.0]
   def change
-    # rubocop:disable Rails/CreateTableWithTimestamps
     create_table :recipes_and_ingredients, id: false do |t|
       t.belongs_to :recipe
       t.belongs_to :ingredient
       t.float :quantity
       t.string :unit
     end
-    # rubocop:enable Rails/CreateTableWithTimestamps
   end
 end
