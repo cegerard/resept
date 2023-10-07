456f789011a72bce8b0dbb73478adc1ad98ffc59
cegerard <git@eriatand.org>
2023-10-07 15:05:07 +0200

    fix cops

--- db/migrate/20220803222000_rename_association_table.rb
+++ db/migrate/20220803222000_rename_association_table.rb
@@ -6,15 +6,12 @@ class RenameAssociationTable < ActiveRecord::Migration[7.0]
     drop_table :recipes_and_ingredients
     # rubocop:enable Rails/ReversibleMigration
 
-    # rubocop:disable Rails/CreateTableWithTimestamps
     create_table :ingredients_recipes, id: false do |t|
       t.belongs_to :recipe
       t.belongs_to :ingredient
       t.float :quantity
       t.string :unit
     end
-    # rubocop:enable Rails/CreateTableWithTimestamps
-
     add_foreign_key :ingredients_recipes, :recipes
     add_foreign_key :ingredients_recipes, :ingredients
   end
