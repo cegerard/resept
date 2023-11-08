686470db16bf031b594bdf8c5d505114c43339cf
cegerard <git@eriatand.org>
2023-11-08 13:31:53 +0100

    Fix tests

--- spec/factories/recipe_ingredients.rb
+++ spec/factories/recipe_ingredients.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :association do
+  factory :recipe_ingredient do
     recipe_id { create(:recipe).id }
     ingredient_id { create(:ingredient).id }
     quantity { Faker::Number.between(from: 0.1, to: 10.0) }
