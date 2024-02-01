d9f4449143a12e1426a7cf2e0c6ba540f0b3599c
cegerard <git@eriatand.org>
2024-02-01 23:11:54 +0100

    Add recipe base data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,52 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Recipe.create({
+                title: 'Pain burger express',
+                description: 'Ce sont des petits pains moelleux et légers, parfaits pour les burgers. Ils sont très faciles à faire et ne nécessitent pas de temps de repos.',
+                url: 'https://saveurhealthy.fr/2020/03/pain-burger-express-ig-bas.html'
+              })
+Recipe.create({
+                title: "Gâteau aux pommes à la farine d'épeautre",
+                description: 'Gateau très léger et gourmand, à la farine d\'épeautre et aux pommes fraiches',
+                url: 'https://cuisinedetouslesjours.com/2012/11/06/gateau-aux-pommes-a-la-farine-depeautre'
+              })
+Recipe.create({
+                title: 'Tourte au thon',
+                description: 'Une recette simple et rapide à réaliser pour un déjeuner ou un diner équilibré',
+                url: 'https://www.federationdesdiabetiques.org/diabete/recettes/tourte-au-thon'
+              })
+Recipe.create({
+                title: 'Tarte express à la noix de coco',
+                description: 'Une recette simple et rapide à réaliser pour un desert ou goûter équilibré',
+                url: 'https://www.marmiton.org/recettes/recette_tarte-express-a-la-noix-de-coco_86233.aspx'
+              })
+Recipe.create({
+                title: 'Gateau aux noix léger',
+                description: 'Un gateau gourmand et léger',
+                url: 'https://www.ptitchef.com/recettes/dessert/gateau-aux-noix-leger-fid-1520863'
+              })
+Recipe.create({
+                title: 'Pain au sarrasin',
+                description: 'Un pain au sarrasin à la machine à pain',
+                url: 'https://www.marmiton.org/recettes/recette_pain-au-sarrasin-a-la-map-machine-a-pain_63574.aspx'
+              })
+Recipe.create({
+                title: 'Boulettes de viandes aux épices',
+                description: 'Une recette simple et rapide à réaliser pour un déjeuner ou un diner équilibré',
+                url: 'https://www.federationdesdiabetiques.org/diabete/recettes/boulettes-de-viandes-aux-epices'
+              })
+Recipe.create({
+                title: 'Muffins pépites de chocolat IG bas',
+                description: 'Ces muffins sont très moelleux. Ils sont moelleux et les pépites raviront tous ceux qui aiment le chocolat',
+                url: 'https://sakmiaim.wordpress.com/2014/08/05/muffins-pepites-de-chocolat-ig-bas-de-marie/'
+              })
+Recipe.create({
+                title: 'Sauce blanche turque',
+                description: 'Cette sauce turque est incontournable pour saucer kebabs, chawarma, viande grillée, koftés et toute garniture que l’on peut servir dans des tortillas ou pains pitas. Elle est très simple à réaliser chez soi, ce qui permet de la parfumer selon nos goûts avec différentes aromates.',
+                url: 'http://www.evasion-culinaire.com/sauce-blanche-turque/#.YUcNdX068wB'
+              })
+Recipe.create({
+                title: 'Tarte amandine aux poires',
+                description: 'A la dégustation, c’est trop bon ! Gourmand, sain, joli, pour le coup cette tarte amandine aux poires est parfaite !',
+                url: 'http://rappelletoidesmets.fr/tarte-amandine-aux-poires-ig-bas/'
+              })
