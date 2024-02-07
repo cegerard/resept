14843dc2e0132db506699a69df1ac766eaba529f
cegerard <git@eriatand.org>
2024-02-07 22:52:08 +0100

    associate burger bread ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,68 +1,16 @@
 # frozen_string_literal: true
 
-# Create default Recipes
-Recipe.create({
-                title: 'Pain burger express',
-                description: 'Ce sont des petits pains moelleux et légers, parfaits pour les burgers. Ils sont très faciles à faire et ne nécessitent pas de temps de repos.',
-                url: 'https://saveurhealthy.fr/2020/03/pain-burger-express-ig-bas.html'
-              })
-Recipe.create({
-                title: "Gâteau aux pommes à la farine d'épeautre",
-                description: 'Gateau très léger et gourmand, à la farine d\'épeautre et aux pommes fraiches',
-                url: 'https://cuisinedetouslesjours.com/2012/11/06/gateau-aux-pommes-a-la-farine-depeautre'
-              })
-Recipe.create({
-                title: 'Tourte au thon',
-                description: 'Une recette simple et rapide à réaliser pour un déjeuner ou un diner équilibré',
-                url: 'https://www.federationdesdiabetiques.org/diabete/recettes/tourte-au-thon'
-              })
-Recipe.create({
-                title: 'Tarte express à la noix de coco',
-                description: 'Une recette simple et rapide à réaliser pour un desert ou goûter équilibré',
-                url: 'https://www.marmiton.org/recettes/recette_tarte-express-a-la-noix-de-coco_86233.aspx'
-              })
-Recipe.create({
-                title: 'Gateau aux noix léger',
-                description: 'Un gateau gourmand et léger',
-                url: 'https://www.ptitchef.com/recettes/dessert/gateau-aux-noix-leger-fid-1520863'
-              })
-Recipe.create({
-                title: 'Pain au sarrasin',
-                description: 'Un pain au sarrasin à la machine à pain',
-                url: 'https://www.marmiton.org/recettes/recette_pain-au-sarrasin-a-la-map-machine-a-pain_63574.aspx'
-              })
-Recipe.create({
-                title: 'Boulettes de viandes aux épices',
-                description: 'Une recette simple et rapide à réaliser pour un déjeuner ou un diner équilibré',
-                url: 'https://www.federationdesdiabetiques.org/diabete/recettes/boulettes-de-viandes-aux-epices'
-              })
-Recipe.create({
-                title: 'Muffins pépites de chocolat IG bas',
-                description: 'Ces muffins sont très moelleux. Ils sont moelleux et les pépites raviront tous ceux qui aiment le chocolat',
-                url: 'https://sakmiaim.wordpress.com/2014/08/05/muffins-pepites-de-chocolat-ig-bas-de-marie/'
-              })
-Recipe.create({
-                title: 'Sauce blanche turque',
-                description: 'Cette sauce turque est incontournable pour saucer kebabs, chawarma, viande grillée, koftés et toute garniture que l’on peut servir dans des tortillas ou pains pitas. Elle est très simple à réaliser chez soi, ce qui permet de la parfumer selon nos goûts avec différentes aromates.',
-                url: 'http://www.evasion-culinaire.com/sauce-blanche-turque/#.YUcNdX068wB'
-              })
-Recipe.create({
-                title: 'Tarte amandine aux poires',
-                description: 'A la dégustation, c’est trop bon ! Gourmand, sain, joli, pour le coup cette tarte amandine aux poires est parfaite !',
-                url: 'http://rappelletoidesmets.fr/tarte-amandine-aux-poires-ig-bas/'
-              })
-
-# Create default Ingredients
-Ingredient.create({ name: 'Farine épautre intégrale' })
-Ingredient.create({ name: 'Farine de lupin' })
-Ingredient.create({ name: 'Farine de sorgho' })
-Ingredient.create({ name: 'Psyllium' })
-Ingredient.create({ name: 'Poudre à lever' })
-Ingredient.create({ name: 'Bicarbonate' })
-Ingredient.create({ name: 'Vinaigre de cidre' })
-Ingredient.create({ name: 'Compote de pomme' })
-Ingredient.create({ name: 'Sel' })
-Ingredient.create({ name: 'Poivre' })
+# Create Ingredients
+spelt_flour = Ingredient.create({ name: 'Farine épautre intégrale' })
+lupin_flour = Ingredient.create({ name: 'Farine de lupin' })
+sorghum_flour = Ingredient.create({ name: 'Farine de sorgho' })
+psyllium = Ingredient.create({ name: 'Psyllium' })
+baking_powder = Ingredient.create({ name: 'Poudre à lever' })
+bicarbonate = Ingredient.create({ name: 'Bicarbonate' })
+cider_vinegar = Ingredient.create({ name: 'Vinaigre de cidre' })
+applesauce = Ingredient.create({ name: 'Compote de pomme' })
+salt = Ingredient.create({ name: 'Sel' })
+pepper = Ingredient.create({ name: 'Poivre' })
 Ingredient.create({ name: 'Pommes moyenne' })
 Ingredient.create({ name: 'Crème liquide' })
 Ingredient.create({ name: 'Rapadura' })
@@ -112,4 +60,73 @@ Ingredient.create({ name: 'Poudre d\'amande' })
 Ingredient.create({ name: 'Rhum' })
 Ingredient.create({ name: 'Amandes effilées' })
 
-# Create default RecipeIngredients
+# Create burger bread Recipe
+burger_bread = Recipe.create({
+                               title: 'Pain burger express',
+                               description: 'Ce sont des petits pains moelleux et légers, parfaits pour les burgers.' \
+                                            'Ils sont très faciles à faire et ne nécessitent pas de temps de repos.',
+                               url: 'https://saveurhealthy.fr/2020/03/pain-burger-express-ig-bas.html'
+                             })
+
+# -- Associate ingredients to the burger bread recipe
+RecipeIngredient.create({ recipe_id: burger_bread.id, ingredient_id: spelt_flour.id, quantity: 85, unit: 'g' })
+RecipeIngredient.create({ recipe_id: burger_bread.id, ingredient_id: lupin_flour.id, quantity: 25, unit: 'g' })
+RecipeIngredient.create({ recipe_id: burger_bread.id, ingredient_id: sorghum_flour.id, quantity: 20, unit: 'g' })
+RecipeIngredient.create({ recipe_id: burger_bread.id, ingredient_id: psyllium.id, quantity: 1, unit: 'cac' })
+RecipeIngredient.create({ recipe_id: burger_bread.id, ingredient_id: baking_powder.id, quantity: 1, unit: 'cas' })
+RecipeIngredient.create({ recipe_id: burger_bread.id, ingredient_id: bicarbonate.id, quantity: 1, unit: 'pincée' })
+RecipeIngredient.create({ recipe_id: burger_bread.id, ingredient_id: cider_vinegar.id, quantity: 1, unit: 'cas' })
+RecipeIngredient.create({ recipe_id: burger_bread.id, ingredient_id: applesauce.id, quantity: 140, unit: 'g' })
+RecipeIngredient.create({ recipe_id: burger_bread.id, ingredient_id: salt.id, quantity: 1, unit: 'pincée' })
+RecipeIngredient.create({ recipe_id: burger_bread.id, ingredient_id: pepper.id, quantity: 1, unit: 'pincée' })
+
+Recipe.create({
+                title: "Gâteau aux pommes à la farine d'épeautre",
+                description: 'Gateau très léger et gourmand, à la farine d\'épeautre et aux pommes fraiches',
+                url: 'https://cuisinedetouslesjours.com/2012/11/06/gateau-aux-pommes-a-la-farine-depeautre'
+              })
+Recipe.create({
+                title: 'Tourte au thon',
+                description: 'Une recette simple et rapide à réaliser pour un déjeuner ou un diner équilibré',
+                url: 'https://www.federationdesdiabetiques.org/diabete/recettes/tourte-au-thon'
+              })
+Recipe.create({
+                title: 'Tarte express à la noix de coco',
+                description: 'Une recette simple et rapide à réaliser pour un desert ou goûter équilibré',
+                url: 'https://www.marmiton.org/recettes/recette_tarte-express-a-la-noix-de-coco_86233.aspx'
+              })
+Recipe.create({
+                title: 'Gateau aux noix léger',
+                description: 'Un gateau gourmand et léger',
+                url: 'https://www.ptitchef.com/recettes/dessert/gateau-aux-noix-leger-fid-1520863'
+              })
+Recipe.create({
+                title: 'Pain au sarrasin',
+                description: 'Un pain au sarrasin à la machine à pain',
+                url: 'https://www.marmiton.org/recettes/recette_pain-au-sarrasin-a-la-map-machine-a-pain_63574.aspx'
+              })
+Recipe.create({
+                title: 'Boulettes de viandes aux épices',
+                description: 'Une recette simple et rapide à réaliser pour un déjeuner ou un diner équilibré',
+                url: 'https://www.federationdesdiabetiques.org/diabete/recettes/boulettes-de-viandes-aux-epices'
+              })
+Recipe.create({
+                title: 'Muffins pépites de chocolat IG bas',
+                description: 'Ces muffins sont très moelleux. Ils sont moelleux et les pépites raviront tous ceux ' \
+                             'qui aiment le chocolat',
+                url: 'https://sakmiaim.wordpress.com/2014/08/05/muffins-pepites-de-chocolat-ig-bas-de-marie/'
+              })
+Recipe.create({
+                title: 'Sauce blanche turque',
+                description: 'Cette sauce turque est incontournable pour saucer kebabs, chawarma, viande grillée, ' \
+                             "koftés et toute garniture que l'on peut servir dans des tortillas ou pains pitas. " \
+                             'Elle est très simple à réaliser chez soi, ce qui permet de la parfumer selon nos ' \
+                             'goûts avec différentes aromates.',
+                url: 'http://www.evasion-culinaire.com/sauce-blanche-turque/#.YUcNdX068wB'
+              })
+Recipe.create({
+                title: 'Tarte amandine aux poires',
+                description: 'A la dégustation, c’est trop bon ! Gourmand, sain, joli, pour le coup cette tarte ' \
+                             'amandine aux poires est parfaite !',
+                url: 'http://rappelletoidesmets.fr/tarte-amandine-aux-poires-ig-bas/'
+              })
